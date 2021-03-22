ac4319265c0b0a16f69d1ac8003d7dc854cdf685
Vishal Singh <VishalSingh1597>
2021-03-23 00:36:44 +0530

    Modified : Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,32 +1,9 @@
-echo "WElcome"
-isFullTime=1
-isParTime=2
-salaryPerHr=20
-PartTimeHr=4
-FulldayHr=8
-random=$(($RANDOM%3))
-if [ $random -eq $isFullTime ]
+echo "Welcome"
+ispresent=1
+randomcheck=$((RANDOM%2))
+if [[ $ispresent -eq $randomcheck ]]
 then
-    Daily_Employee_Wage=$(( $salaryPerHr * $FulldayHr ))
-    echo "Daily wage of Fulltime Employee is:" $Daily_Employee_Wage
-elif [ $random -eq $isParTime ]
-then
-    Daily_Employee_Wage=$(( $salaryPerHr * $PartTimeHr ))
-    echo "Daily wage of Parttime Employee is:" $Daily_Employee_Wage
+      echo "Employee is Present"
 else
-    echo "Employee is Absent"
+      echo "Employee is Absent"
 fi
-
-case $random in
-     $isFullTime)
-          emp_hrs=8
-          ;;
-     $isPartTime)
-          emp_hrs=4
-          ;;
-     *)
-          emp_hrs=0
-          ;;
-esac
-Daily_Employee_Wage=$(($emp_hrs*$salaryPerHr ))
-echo "Daily wage of Employee is:" $Daily_Employee_Wage
